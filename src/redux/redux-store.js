c53412f58d4c15bd1506a5615411af41714eee2b
khrushchik <xotta6bi4dzin@gmail.com>
2022-05-03 17:53:37 +0300

    new commit

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,12 +2,14 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import dialogsReducer from "./dialogs-reducer";
+import usersReducer from "./users-reducer";
 
 let reducers = combineReducers(
     {
         profilePage: profileReducer,
         sidebar: sidebarReducer,
-        dialogPage: dialogsReducer
+        dialogPage: dialogsReducer,
+        usersPage: usersReducer
     });
 
 let store = createStore(reducers);
