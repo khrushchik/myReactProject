ad57fc14aaa83ca58148e788ba900bfa97ea433d
khrushchik <xotta6bi4dzin@gmail.com>
2022-05-09 16:24:27 +0300

    new commit

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from './ProfileStatus'
+import userPhoto from "../../../assets/images/user-icon.png";
 
 const ProfileInfo = (props) => {
 
@@ -15,6 +16,9 @@ const ProfileInfo = (props) => {
             <img
                 src='https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300'/>
 */}
+            <div className={styles.item}>
+                <img src={props.profile.photos.small != null ? props.profile.photos.small : userPhoto}/>
+            </div>
             <div className={styles.nameBlock}>
                 {props.profile.fullName}
             </div>
